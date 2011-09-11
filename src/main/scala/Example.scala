e6a232ab0164b9983cc9f43d08696b1fd4bc6174
Peter Hausel <peter_kovac11@hotmail.com>
2011-09-11 00:26:26 -0400

    add basic implementation

--- src/main/scala/Example.scala
+++ src/main/scala/Example.scala
@@ -2,32 +2,39 @@ package com.example
 
 import unfiltered.request._
 import unfiltered.response._
-
+import util._
 import org.clapper.avsl.Logger
 
 /** unfiltered plan */
 class Scalatra extends unfiltered.filter.Plan {
 
-  private val handlers = collection.mutable.Map[String,Function1[HttpResponse]]
+  private val handlers = collection.mutable.Map[String,Function0[AnyRef]]()
 
   protected val _request    = new DynamicVariable[HttpRequest[_]](null)
 
+  def get(r:String)( f: => AnyRef) = {
+    val p = () => f
+    handlers += (r -> p)
+  }
   implicit def request = _request value
 
-  protected def executeRoutes =  {
-    
+  protected def executeRoutes(req: HttpRequest[_]):ResponseFunction[javax.servlet.http.HttpServletResponse] =  {
+    //proper matching logic should come here, for now it's very simplistic
+    val handler = handlers.keys.filter(req.uri.startsWith(_))
+    handler.headOption.map(handlers(_).asInstanceOf[ResponseFunction[javax.servlet.http.HttpServletResponse]]).getOrElse ( NotFound ~> ResponseString("could not find handler"))
   }
   val logger = Logger(classOf[App])
 
   def intent = {
     case req @ _  =>  _request.withValue(req) {
-        executeRoutes
+        executeRoutes(req)
     }
-    
   }
 }
 class App extends Scalatra {
-  
+  get ("/hello") {
+    Ok ~> ResponseString("hello world, hello:"+request.toString)
+  }
 }
 
 
